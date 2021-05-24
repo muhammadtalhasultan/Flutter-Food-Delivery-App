5ed49c452f6745b2ec9254357387df5af0328e6c
jing <maliktalha0101@gmail.com>
2021-05-24 22:00:11 +0800

    episode 8

--- lib/screens/offerScreen.dart
+++ lib/screens/offerScreen.dart
@@ -1,9 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:monkey_app_demo/const/colors.dart';
+import 'package:monkey_app_demo/utils/helper.dart';
+import 'package:monkey_app_demo/widgets/customNavBar.dart';
 
 class OfferScreen extends StatelessWidget {
   static const routeName = "/offerScreen";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Container(
+              height: Helper.getScreenHeight(context),
+              width: Helper.getScreenWidth(context),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Latest Offers",
+                            style: Helper.getTheme(context).headline5,
+                          ),
+                          Image.asset(
+                            Helper.getAssetName("cart.png", "virtual"),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [Text("Find discounts, Offer special")],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: Helper.getScreenWidth(context) * 0.4,
+                            child: ElevatedButton(
+                                onPressed: () {}, child: Text("Check Offers")),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("breakfast.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Cafe de Noires",
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("western2.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Isso",
+                    ),
+                    OfferCard(
+                      image: Image.asset(
+                        Helper.getAssetName("coffee3.jpg", "real"),
+                        fit: BoxFit.cover,
+                      ),
+                      name: "Cafe Beans",
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: CustomNavBar(
+              offer: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class OfferCard extends StatelessWidget {
+  const OfferCard({
+    Key key,
+    String name,
+    Image image,
+  })  : _image = image,
+        _name = name,
+        super(key: key);
+
+  final String _name;
+  final Image _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(height: 200, width: double.infinity, child: _image),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Text(
+                  _name,
+                  style: Helper.getTheme(context)
+                      .headline4
+                      .copyWith(color: AppColor.primary),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  Helper.getAssetName("star_filled.png", "virtual"),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  "4.9",
+                  style: TextStyle(
+                    color: AppColor.orange,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text("(124 ratings) Cafe"),
+                SizedBox(
+                  width: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    ".",
+                    style: TextStyle(
+                        color: AppColor.orange, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(" Western Food"),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -1,9 +1,209 @@
 import 'package:flutter/material.dart';
+import 'package:monkey_app_demo/const/colors.dart';
+import 'package:monkey_app_demo/utils/helper.dart';
+import 'package:monkey_app_demo/widgets/customNavBar.dart';
+import 'package:monkey_app_demo/widgets/customTextInput.dart';
 
 class ProfileScreen extends StatelessWidget {
   static const routeName = "/profileScreen";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Container(
+              height: Helper.getScreenHeight(context),
+              width: Helper.getScreenWidth(context),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Profile",
+                            style: Helper.getTheme(context).headline5,
+                          ),
+                          Image.asset(
+                            Helper.getAssetName("cart.png", "virtual"),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ClipOval(
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 80,
+                              width: 80,
+                              child: Image.asset(
+                                Helper.getAssetName(
+                                  "user.jpg",
+                                  "real",
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              child: Container(
+                                height: 20,
+                                width: 80,
+                                color: Colors.black.withOpacity(0.3),
+                                child: Image.asset(Helper.getAssetName(
+                                    "camera.png", "virtual")),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            Helper.getAssetName("edit_filled.png", "virtual"),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "Edit Profile",
+                            style: TextStyle(color: AppColor.orange),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Hi there Emilia!",
+                        style: Helper.getTheme(context).headline4.copyWith(
+                              color: AppColor.primary,
+                            ),
+                      ),
+                      Text("Sign Out"),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      CustomFormImput(
+                        label: "Name",
+                        value: "Emilia Clarke",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormImput(
+                        label: "Email",
+                        value: "emiliaclarke@email.com",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormImput(
+                        label: "Mobile No",
+                        value: "emiliaclarke@email.com",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormImput(
+                        label: "Address",
+                        value: "No 23, 6th Lane, Colombo 03",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormImput(
+                        label: "Password",
+                        value: "Emilia Clarke",
+                        isPassword: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormImput(
+                        label: "Confirm Password",
+                        value: "Emilia Clarke",
+                        isPassword: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Save"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: CustomNavBar(
+              profile: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomFormImput extends StatelessWidget {
+  const CustomFormImput({
+    Key key,
+    String label,
+    String value,
+    bool isPassword = false,
+  })  : _label = label,
+        _value = value,
+        _isPassword = isPassword,
+        super(key: key);
+
+  final String _label;
+  final String _value;
+  final bool _isPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      padding: const EdgeInsets.only(left: 40),
+      decoration: ShapeDecoration(
+        shape: StadiumBorder(),
+        color: AppColor.placeholderBg,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          labelText: _label,
+          contentPadding: const EdgeInsets.only(
+            top: 10,
+            bottom: 10,
+          ),
+        ),
+        obscureText: _isPassword,
+        initialValue: _value,
+        style: TextStyle(
+          fontSize: 14,
+        ),
+      ),
+    );
   }
 }
